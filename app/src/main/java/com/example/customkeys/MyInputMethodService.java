5c5900e6bd2c2cb50f7c09fa20802a42378817c4
ddaiki5 <62028697+ddaiki5@users.noreply.github.com>
2022-01-20 04:05:40 +0900

    add: 選択モードキーを実装しました #5

--- app/src/main/java/com/example/customkeys/MyInputMethodService.java
+++ app/src/main/java/com/example/customkeys/MyInputMethodService.java
@@ -3,6 +3,8 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Selection;
 import android.util.Log;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
@@ -10,8 +12,12 @@ import android.inputmethodservice.KeyboardView;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.ExtractedText;
+import android.view.inputmethod.ExtractedTextRequest;
 import android.view.inputmethod.InputConnection;
 import android.text.TextUtils;
+
+import java.text.BreakIterator;
 
 public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
 
@@ -19,8 +25,13 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
     private Keyboard keyboard;
 
     private ClipboardManager clipboardManager;
+    private Selection selection;
 
     private boolean caps = false;
+    private boolean isSelectionMode = false;
+    private int relativeCursol = 0;
+
+
 
     @Override
     public View onCreateInputView() {
@@ -30,6 +41,12 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
         keyboardView.setOnKeyboardActionListener(this);
         clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
         return keyboardView;
+    }
+
+    @Override
+    public View onCreateCandidatesView(){
+        //ここで候補を表示するViewを定義する
+        return null;
     }
 
     @Override
@@ -100,17 +117,32 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
                 case CustomCode.KEYCODE_REDO:
                     break;
                 case CustomCode.KEYCODE_REGION:
-                    //inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_));
+                    isSelectionMode = !isSelectionMode;
                     break;
                 case CustomCode.KEYCODE_TAB:
                     //inputConnection.commitText("\t", 1);
                     inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_TAB));
                     break;
                 case CustomCode.KEYCODE_LEFTCURSOR:
-                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT));
+                    if(isSelectionMode) {
+                        ExtractedText leftExtractedText = inputConnection.getExtractedText(new ExtractedTextRequest(), 0);
+                        int leftStartIndex = leftExtractedText.startOffset + leftExtractedText.selectionStart;
+                        int leftEndIndex = leftExtractedText.startOffset + leftExtractedText.selectionEnd;
+                        inputConnection.setSelection(leftStartIndex, leftEndIndex - 1);
+                    }else{
+                        inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT));
+                    }
                     break;
                 case CustomCode.KEYCODE_RIGHTCURSOR:
-                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT));
+
+                    if(isSelectionMode) {
+                        ExtractedText rightExtractedText = inputConnection.getExtractedText(new ExtractedTextRequest(), 0);
+                        int rightStartIndex = rightExtractedText.startOffset + rightExtractedText.selectionStart;
+                        int rightEndIndex = rightExtractedText.startOffset + rightExtractedText.selectionEnd;
+                        inputConnection.setSelection(rightStartIndex, rightEndIndex + 1);
+                    }else{
+                        inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT));
+                    }
                     break;
                 case CustomCode.KEYCODE_DIACRITIC:
                     CharSequence diacritic_target = inputConnection.getTextBeforeCursor(1, 0);
@@ -134,6 +166,7 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
                             code = Character.toUpperCase(code);
                         }
                         inputConnection.commitText(String.valueOf(code), 1);
+
                     }
             }
         }
